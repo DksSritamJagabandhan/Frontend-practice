356c8cd806e2e5c46d74579707f320952c7f79a4
DksSritamJagabandhan <dkssritamjagabandhan@gmail.com>
2026-07-15 12:13:57 +0530

    js

--- Basics/prog.js
+++ Basics/prog.js
@@ -2,26 +2,24 @@ console.log("Rohan");
 console.log(typeof "rohan")
 console.log(typeof undefined)
 console.log(typeof null)
-//datatype conversion
+// //datatype conversion
 let mark = "23"
 console.log(typeof mark)
 let numMark = Number(mark)
 console.log(typeof numMark)
 
-const id=Symbol("Rohan")
-const anotherId=Symbol("Rohan")
-console.log(id===anotherId)
-//array
-const fruits=["Apple","Orange","Banana"]
-//object
-let obj={
-    name: "Rohan",
-    age: 23,
-}
-//function
-const myFunction=function(){
-    console.log(fruits);
-
-
-}
-myFunction()
+// // const id=Symbol("Rohan")
+// // const anotherId=Symbol("Rohan")
+// // console.log(id===anotherId)
+// // //array
+// const fruits=["Apple","Orange","Banana"]
+// // //object
+// // let obj={
+// //     name: "Rohan",
+// //     age: 23,
+// // }
+// //function
+// const myFunction=function(){
+//     console.log(fruits);
+// }
+// myFunction()
---
+++ Basics/varConstLet.js
@@ -0,0 +1,10 @@
+const id=8822
+var name="Rohan" // normally var can not use in javasvript because it occur problems in scope 
+let address="bhubaneswar"
+
+// id=3433 const can not change
+name="rahul"
+address="cuttack"
+
+console.table([id,name,address])
+// if any value is not assign then it show undefine

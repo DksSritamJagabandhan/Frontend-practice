48548c59010dcf14c7cdac916c4c8f9728ab3946
DksSritamJagabandhan <dkssritamjagabandhan@gmail.com>
2026-07-17 11:57:55 +0530

    initialize

--- Basics/prog.js
+++ Basics/prog.js
@@ -23,3 +23,14 @@ console.log(typeof numMark)
 //     console.log(fruits);
 // }
 // myFunction()
+
+const id=8822
+var name="Rohan" // normally var can not use in javasvript because it occur problems in scope 
+let address="bhubaneswar"
+
+// id=3433 const can not change
+name="rahul"
+address="cuttack"
+
+console.table([id,name,address])
+// if any value is not assign then it show undefine
